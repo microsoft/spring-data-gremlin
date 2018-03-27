9de2c8eb02e93a2e8505ee3536d5d74ec53228e0
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-03-27 14:24:24 +0800

    Add one Annotation @Vetrex for domain class. (#3)
    * Annotation @Vertex is for domain class, indicates one Vertex in graph.
    
    * It has one optional label field, will return short name of class by default.
    
    * Create one Class integrate Constants variables.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

---
+++ src/main/java/com/microsoft/spring/data/gremlin/common/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.common;
+
+public class Constants {
+    private Constants() {
+        // Hide default constructor
+    }
+
+    public static final String DEFAULT_VERTEX_LABEL = "";
+}
