d2dc345a7e0305f88734223acf25d5c23b9435cd
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-03-28 14:24:07 +0800

    Support properties configuration (#7)
    * Support properties configuration from resources.
    
    * Add endpoint, port, username and password properties.
    
    * Add Unit test for gremlin propertiesConfiguration.
    
    * Use dependency management for springframework and spring-boot
    
    * Use springframework bom with Version 5.0.1.RELEASE
    
    * Use spring-boot-dependencies with Version 2.0.0.RELEASE
    
    Signed-off-by: Pan Li <panli@microsoft.com>

---
+++ src/main/java/com/microsoft/spring/data/gremlin/common/GremlinPropertiesConfiguration.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.common;
+
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "gremlin")
+public class GremlinPropertiesConfiguration {
+
+    private String endpoint;
+
+    private String port;
+
+    private String username;
+
+    private String password;
+}
