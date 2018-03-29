7434285e798d23ca9bdfc303f9c3c9ef6fa61f89
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-03-29 11:30:51 +0800

    Implement GremlinScriptVertexScriptLiteral. (#21)
    * Implement GremlinScriptVertexScriptLiteral.
    
    * Add abstract class for generate all properties, support String/Integer/Boolean type.
    
    * Implement Script for Vertex Literal.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/microsoft/spring/data/gremlin/conversion/GremlinVertexScriptLiteral.java
+++ src/main/java/com/microsoft/spring/data/gremlin/conversion/GremlinVertexScriptLiteral.java
@@ -5,14 +5,34 @@
  */
 package com.microsoft.spring.data.gremlin.conversion;
 
+import com.microsoft.spring.data.gremlin.common.Constants;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @NoArgsConstructor
-public class GremlinVertexScriptLiteral implements GremlinScript<String> {
+public class GremlinVertexScriptLiteral extends GremlinScriptPropertiesLiteral implements GremlinScript<String> {
 
     @Override
-    public String generateScript(GremlinSource gremlinSource) {
-        throw new NotImplementedException("generate literal vertex script is not implemented");
+    public String generateScript(GremlinSource source) {
+        final List<String> scriptList = new ArrayList<>();
+        final String label = source.getLabel();
+        final String id = source.getId();
+        final Map<String, Object> properties = source.getProperties();
+
+        Assert.notNull(label, "label should not be null");
+        Assert.notNull(id, "id should not be null");
+        Assert.notNull(properties, "properties should not be null");
+
+        scriptList.add(Constants.GREMLIN_PRIMITIVE_GRAPH);
+        scriptList.add(String.format(Constants.GREMLIN_PRIMITIVE_ADD_VERTEX, label));
+        scriptList.add(String.format(Constants.GREMLIN_PRIMITIVE_PROPERTY_STRING, Constants.PROPERTY_ID, id));
+
+        super.generateGremlinScriptProperties(scriptList, properties);
+
+        return String.join(Constants.GREMLIN_PRIMITIVE_INVOKE, scriptList);
     }
 }
