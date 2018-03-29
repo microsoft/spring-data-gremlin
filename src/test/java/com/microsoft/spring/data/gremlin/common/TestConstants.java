1efcf49242f5da90e44bb0ae76b2b9d97309be1f
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-03-29 22:02:46 +0800

    Add domain class of one network graph for test. (#11)
    * There is one graph named network.
    
    * Define Person and Project as Vertex.
    
    * Define Relationship as Edge.
    
    Signed-off-by: Pan Li <panli@microsoft.com>
    Signed-off-by: Weiping Pan <Weiping.Pan@microsoft.com>

---
+++ src/test/java/com/microsoft/spring/data/gremlin/common/TestConstants.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.common;
+
+public class TestConstants {
+    private TestConstants() {
+        // Hide default constructor
+    }
+
+    public static final String VERTEX_PERSON_LABEL = "label-person";
+    public static final String VERTEX_PROJECT_LABEL = "label-project";
+    public static final String EDGE_RELATIONSHIP_LABEL = "label-relationship";
+}
