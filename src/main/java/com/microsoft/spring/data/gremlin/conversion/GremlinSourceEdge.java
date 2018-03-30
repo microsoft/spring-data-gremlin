2cf37375c72026a799d0b0b331d3fc2727cf2018
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-03-30 17:44:24 +0800

    Implement GremlinSource interface. (#26)
    * Implement GremlinSource with Vertex, Edge and Graph Source
    
    * Add one new field GremlinSourceWriter to Source.
    
    Signed-off-by: Pan Li <panli@microsoft.com>
    Signed-off-by: Weiping Pan <Weiping.Pan@microsoft.com>

---
+++ src/main/java/com/microsoft/spring/data/gremlin/conversion/GremlinSourceEdge.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.conversion;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class GremlinSourceEdge extends BasicGremlinSource<String> {
+
+    @Getter
+    @Setter
+    private String vertexIdFrom;
+
+    @Getter
+    @Setter
+    private String vertexIdTo;
+
+    public GremlinSourceEdge() {
+        super();
+    }
+}
---
+++ src/main/java/com/microsoft/spring/data/gremlin/exception/UnexpectedGremlinSourceTypeException.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.exception;
+
+import org.springframework.lang.Nullable;
+
+public class UnexpectedGremlinSourceTypeException extends IllegalStateException {
+
+    public UnexpectedGremlinSourceTypeException(String msg) {
+        super(msg);
+    }
+
+    public UnexpectedGremlinSourceTypeException(@Nullable  String msg, @Nullable Throwable cause) {
+        super(msg, cause);
+    }
+}
