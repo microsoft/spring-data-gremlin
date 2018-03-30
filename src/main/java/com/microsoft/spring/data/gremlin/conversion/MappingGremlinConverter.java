3c448f0bdf389226512d2312ca9d3cc45bf4cb47
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-03-30 17:48:49 +0800

    Add MappingGremlinConverter for domain class. (#22)
    * Add MappingGremlinConverter for domain class.
    
    Signed-off-by: Pan Li <panli@microsoft.com>
    Signed-off-by: Weiping Pan <Weiping.Pan@microsoft.com>

---
+++ src/main/java/com/microsoft/spring/data/gremlin/conversion/MappingGremlinConverter.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.conversion;
+
+import com.microsoft.spring.data.gremlin.mapping.GremlinPersistentEntity;
+import com.microsoft.spring.data.gremlin.mapping.GremlinPersistentProperty;
+import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.data.convert.EntityConverter;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.lang.NonNull;
+
+public class MappingGremlinConverter
+        implements EntityConverter<GremlinPersistentEntity<?>, GremlinPersistentProperty, Object, GremlinSource>,
+        ApplicationContextAware {
+
+    protected final MappingContext<? extends GremlinPersistentEntity<?>, GremlinPersistentProperty> mappingContext;
+    protected GenericConversionService conversionService;
+    private ApplicationContext applicationContext;
+
+    public MappingGremlinConverter(
+            MappingContext<? extends GremlinPersistentEntity<?>, GremlinPersistentProperty> context) {
+
+        this.mappingContext = context;
+        this.conversionService = new GenericConversionService();
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return this.applicationContext;
+    }
+
+    @Override
+    public MappingContext<? extends GremlinPersistentEntity<?>, GremlinPersistentProperty> getMappingContext() {
+        return this.mappingContext;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) {
+        this.applicationContext = context;
+    }
+
+    @Override
+    public ConversionService getConversionService() {
+        return this.conversionService;
+    }
+
+    @Override
+    public <R extends Object> R read(Class<R> type, @NonNull GremlinSource source) {
+        throw new NotImplementedException("read method of MappingGremlinConverter not implemented yet");
+    }
+
+    @Override
+    public void write(@NonNull Object domain, @NonNull GremlinSource source) {
+        throw new NotImplementedException("write method of MappingGremlinConverter not implemented yet");
+    }
+}
+
