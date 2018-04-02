b00492d12d0fcc8bf86a05feaee61a885e0c430d
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-04-02 11:39:08 +0800

    Implement GremlinOperations interface (#29)
    * Implemented deleteAll and insert.
    
    Signed-off-by: Pan Li <panli@microsoft.com>
    Signed-off-by: Weiping Pan <Weiping.Pan@microsoft.com>

---
+++ src/main/java/com/microsoft/spring/data/gremlin/query/GremlinTemplate.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.query;
+
+import com.microsoft.spring.data.gremlin.common.GremlinFactory;
+import com.microsoft.spring.data.gremlin.conversion.GremlinScriptFactory;
+import com.microsoft.spring.data.gremlin.conversion.GremlinSource;
+import com.microsoft.spring.data.gremlin.exception.GremlinInsertionException;
+import com.microsoft.spring.data.gremlin.repository.support.GremlinEntityInformation;
+import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Result;
+import org.apache.tinkerpop.gremlin.driver.exception.ResponseException;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+public class GremlinTemplate implements GremlinOperations, ApplicationContextAware {
+
+    private final GremlinFactory gremlinFactory;
+    private final MappingGremlinConverter mappingConverter;
+    private ApplicationContext context;
+
+    public GremlinTemplate(@NonNull GremlinFactory factory, @NonNull MappingGremlinConverter converter) {
+        this.gremlinFactory = factory;
+        this.mappingConverter = converter;
+    }
+
+    @Override
+    public void setApplicationContext(@NonNull ApplicationContext context) throws BeansException {
+        this.context = context;
+    }
+
+    @Override
+    public void deleteAll() {
+        final Client client = this.gremlinFactory.getGremlinClient();
+
+        client.submit(new GremlinScriptEdgeDropLiteral().generateScript(null)).all().join();
+        client.submit(new GremlinScriptVertexDropLiteral().generateScript(null)).all().join();
+    }
+
+    @Override
+    public <T> T insert(T object) {
+        final Client client = this.gremlinFactory.getGremlinClient();
+        final GremlinEntityInformation information = new GremlinEntityInformation(object.getClass());
+        final GremlinSource source = information.getGremlinSource();
+        final GremlinScriptFactory scriptFactory = information.getGremlinScriptFactory();
+
+        try {
+            client.submit(scriptFactory.createGremlinScript().generateScript(source)).all().join();
+        } catch (ResponseException e) {
+            final String typeName = object.getClass().getName();
+            throw new GremlinInsertionException(String.format("unable to insert type %s from gremlin", typeName), e);
+        }
+
+        return object;
+    }
+}
