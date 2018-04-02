6994f6591c72f66b8b40fcd34a57a3ae80f6efb6
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-04-02 13:31:29 +0800

    Add IT test for GremlinTemplate (#37)
    * Add vertex deleteAll.
    
    Signed-off-by: Pan Li <panli@microsoft.com>
    Signed-off-by: Catherine Zhu <yungez@microsoft.com>

--- src/test/java/com/microsoft/spring/data/gremlin/common/TestConstants.java
+++ src/test/java/com/microsoft/spring/data/gremlin/common/TestConstants.java
@@ -19,4 +19,37 @@ public class TestConstants {
     public static final String VERTEX_PERSON_LABEL = "label-person";
     public static final String VERTEX_PROJECT_LABEL = "label-project";
     public static final String EDGE_RELATIONSHIP_LABEL = "label-relationship";
+
+    public static final String VERTEX_PERSON_ID = "233333";
+    public static final String VERTEX_PERSON_NAME = "incarnation-p-lee";
+
+    public static final String VERTEX_PERSON_0_ID = "000000";
+    public static final String VERTEX_PERSON_0_NAME = "silencer";
+
+    public static final String VERTEX_PERSON_1_ID = "111111";
+    public static final String VERTEX_PERSON_1_NAME = "templar-assassin";
+
+    public static final String VERTEX_PROJECT_ID = "666666";
+    public static final String VERTEX_PROJECT_NAME = "spring-data-gremlin";
+    public static final String VERTEX_PROJECT_URI = "https://github.com/Incarnation-p-lee/spring-data-gremlin.git";
+
+    public static final String VERTEX_PROJECT_0_ID = "222222";
+    public static final String VERTEX_PROJECT_0_NAME = "spring-data-documentdb";
+    public static final String VERTEX_PROJECT_0_URI = "https://github.com/Microsoft/spring-data-documentdb";
+
+    public static final String EDGE_RELATIONSHIP_ID = "999999";
+    public static final String EDGE_RELATIONSHIP_NAME = "created";
+    public static final String EDGE_RELATIONSHIP_LOCATION = "shanghai";
+
+    public static final String EDGE_RELATIONSHIP_0_ID = "333333";
+    public static final String EDGE_RELATIONSHIP_0_NAME = "contributed";
+    public static final String EDGE_RELATIONSHIP_0_LOCATION = "war3";
+
+    public static final String EDGE_RELATIONSHIP_1_ID = "444444";
+    public static final String EDGE_RELATIONSHIP_1_NAME = "contributed";
+    public static final String EDGE_RELATIONSHIP_1_LOCATION = "dota";
+
+    public static final String EDGE_RELATIONSHIP_2_ID = "555555";
+    public static final String EDGE_RELATIONSHIP_2_NAME = "create";
+    public static final String EDGE_RELATIONSHIP_2_LOCATION = "shanghai";
 }
