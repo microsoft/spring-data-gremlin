58e7339e474fc0a5618a6431647d5ad123843d70
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-04-04 23:00:08 +0800

    Add interface GremlinSourceReader. (#47)
    * Add interface GremlinSourceReader.
    
    * Implement the interface with GremlinSourceVertexReader
    
    * Add createInstance to util.
    
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>
    Signed-off-by: Weiping Pan <Weiping.Pan@microsoft.com>

---
+++ src/main/java/com/microsoft/spring/data/gremlin/conversion/GremlinSourceReader.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.conversion;
+
+/**
+ * Provider Entity type dependent read method.
+ */
+public interface GremlinSourceReader {
+    /**
+     * Read data from GremlinSource to domain
+     */
+    <T extends Object> T read(Class<T> type, MappingGremlinConverter converter, GremlinSource source);
+}
