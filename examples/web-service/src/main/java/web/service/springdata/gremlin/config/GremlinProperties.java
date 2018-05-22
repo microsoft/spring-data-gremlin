ee823f4679ea4355f8ebbd2d39da84dfd3a30194
Pan Li <panli@microsoft.com>
2018-05-22 14:07:25 +0800

    Add example code for gremlin
    * examples contains 2 submodules, example and web-service
    
    Signed-off-by: Pan Li <panli@microsoft.com>

---
+++ examples/web-service/src/main/java/web/service/springdata/gremlin/config/GremlinProperties.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package web.service.springdata.gremlin.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "gremlin")
+public class GremlinProperties {
+
+    private String endpoint;
+
+    private String port;
+
+    private String username;
+
+    private String password;
+}
