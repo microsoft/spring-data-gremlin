7fff215a5e4eedb681e60a4b4d42007ca210c42b
Pan Li <panli@microsoft.com>
2018-06-07 15:08:24 +0800

    convert date type in domain to milliseconds.
    * Store milliseconds of domain in database.
    
    * only number type is comparable.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/microsoft/spring/data/gremlin/conversion/script/AbstractGremlinScriptLiteral.java
+++ src/main/java/com/microsoft/spring/data/gremlin/conversion/script/AbstractGremlinScriptLiteral.java
@@ -5,28 +5,34 @@
  */
 package com.microsoft.spring.data.gremlin.conversion.script;
 
-import com.microsoft.spring.data.gremlin.common.Constants;
 import com.microsoft.spring.data.gremlin.common.GremlinUtils;
 import com.microsoft.spring.data.gremlin.exception.GremlinUnexpectedEntityTypeException;
 import org.apache.tinkerpop.shaded.jackson.core.JsonProcessingException;
 import org.springframework.lang.NonNull;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+
+import static com.microsoft.spring.data.gremlin.common.Constants.*;
 
 public abstract class AbstractGremlinScriptLiteral {
 
     private static String generateProperty(@NonNull String name, @NonNull String value) {
-        return String.format(Constants.GREMLIN_PRIMITIVE_PROPERTY_STRING, name, value);
+        return String.format(GREMLIN_PRIMITIVE_PROPERTY_STRING, name, value);
     }
 
     private static String generateProperty(@NonNull String name, @NonNull Integer value) {
-        return String.format(Constants.GREMLIN_PRIMITIVE_PROPERTY_NUMBER, name, value);
+        return String.format(GREMLIN_PRIMITIVE_PROPERTY_NUMBER, name, value);
     }
 
     private static String generateProperty(@NonNull String name, @NonNull Boolean value) {
-        return String.format(Constants.GREMLIN_PRIMITIVE_PROPERTY_BOOLEAN, name, value);
+        return String.format(GREMLIN_PRIMITIVE_PROPERTY_BOOLEAN, name, value);
+    }
+
+    private static String generateProperty(@NonNull String name, @NonNull Long value) {
+        return String.format(GREMLIN_PRIMITIVE_PROPERTY_NUMBER, name, value);
     }
 
     private static String generateProperty(@NonNull String name, @NonNull Object value) {
@@ -37,6 +43,8 @@ public abstract class AbstractGremlinScriptLiteral {
             return generateProperty(name, (Boolean) value);
         } else if (value instanceof String) {
             return generateProperty(name, (String) value);
+        } else if (value instanceof Date) {
+            return generateProperty(name, GremlinUtils.timeToMilliSeconds(value));
         } else {
             final String propertyScript;
 
@@ -59,15 +67,19 @@ public abstract class AbstractGremlinScriptLiteral {
     }
 
     private static String generateHas(@NonNull String name, @NonNull Integer value) {
-        return String.format(Constants.GREMLIN_PRIMITIVE_HAS_NUMBER, name, value);
+        return String.format(GREMLIN_PRIMITIVE_HAS_NUMBER, name, value);
     }
 
     private static String generateHas(@NonNull String name, @NonNull Boolean value) {
-        return String.format(Constants.GREMLIN_PRIMITIVE_HAS_BOOLEAN, name, value);
+        return String.format(GREMLIN_PRIMITIVE_HAS_BOOLEAN, name, value);
     }
 
     private static String generateHas(@NonNull String name, @NonNull String value) {
-        return String.format(Constants.GREMLIN_PRIMITIVE_HAS_STRING, name, value);
+        return String.format(GREMLIN_PRIMITIVE_HAS_STRING, name, value);
+    }
+
+    private static String generateHas(@NonNull String name, @NonNull Long value) {
+        return String.format(GREMLIN_PRIMITIVE_HAS_NUMBER, name, value);
     }
 
     public static String generateHas(@NonNull String name, @NonNull Object value) {
@@ -78,6 +90,8 @@ public abstract class AbstractGremlinScriptLiteral {
             return generateHas(name, (Boolean) value);
         } else if (value instanceof String) {
             return generateHas(name, (String) value);
+        } else if (value instanceof Date) {
+            return generateHas(name, GremlinUtils.timeToMilliSeconds(value));
         } else {
             final String hasScript;
 
--- src/main/java/com/microsoft/spring/data/gremlin/conversion/source/AbstractGremlinSourceReader.java
+++ src/main/java/com/microsoft/spring/data/gremlin/conversion/source/AbstractGremlinSourceReader.java
@@ -11,8 +11,10 @@ import org.apache.tinkerpop.shaded.jackson.databind.JavaType;
 import org.apache.tinkerpop.shaded.jackson.databind.type.TypeFactory;
 import org.springframework.data.mapping.PersistentProperty;
 import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
 
 import java.io.IOException;
+import java.util.Date;
 
 public abstract class AbstractGremlinSourceReader {
 
@@ -24,6 +26,9 @@ public abstract class AbstractGremlinSourceReader {
                 || type == Boolean.class || type == boolean.class
                 || type == String.class) {
             return value;
+        } else if (type == Date.class) {
+            Assert.isTrue(value instanceof Long, "Date store value must be instance of long");
+            return new Date((Long) value);
         } else {
             final Object object;
 
