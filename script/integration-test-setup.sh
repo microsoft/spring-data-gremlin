1380558d499c38c5ad4cbb7f5cce27b894b7db33
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-09-17 11:43:21 +0800

    Add integration test for integer (#151)
    * Add IT test for id with Long type.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- script/integration-test-setup.sh
+++ script/integration-test-setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cat << EOF
+
 gremlin.endpoint=localhost
 gremlin.port=8889
-gremlin.username=\${your-username-if-need}
-gremlin.password=\${your-password-if-need}
+gremlin.username="anonymous"
+gremlin.password="anonymous"
 
 EOF
 
