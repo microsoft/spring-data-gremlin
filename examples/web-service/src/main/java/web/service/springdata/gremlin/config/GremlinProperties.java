0cd93a93243c8d1282aa8b17f46b389e0e069a8f
Fabien Coppens <fabien_coppens@spe.sony.com>
2018-11-06 13:28:28 -0800

    Update example add to include serializer in its config

--- examples/web-service/src/main/java/web/service/springdata/gremlin/config/GremlinProperties.java
+++ examples/web-service/src/main/java/web/service/springdata/gremlin/config/GremlinProperties.java
@@ -9,6 +9,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import org.apache.tinkerpop.gremlin.driver.ser.Serializers;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
@@ -28,4 +30,6 @@ public class GremlinProperties {
     private boolean sslEnabled;
 
     private boolean telemetryAllowed = true;
+    
+    private String serializer = Serializers.GRAPHSON.toString();
 }
