56f3fff7b6072a42b5f653b2529ff494b9a0cc70
Tin Nguyen <ntin87@gmail.com>
2019-09-28 18:16:13 -0400

    Removed unused imports

--- src/main/java/com/microsoft/spring/data/gremlin/conversion/result/AbstractGremlinResultReader.java
+++ src/main/java/com/microsoft/spring/data/gremlin/conversion/result/AbstractGremlinResultReader.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import static com.microsoft.spring.data.gremlin.common.Constants.*;
 import static com.microsoft.spring.data.gremlin.common.Constants.PROPERTY_VALUE_WITH_AT;
 
 @NoArgsConstructor
